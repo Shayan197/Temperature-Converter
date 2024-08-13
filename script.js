32b6a386d486825b5d8195c63c0b5b4e4eafcb17
Shayan Bukhari <shayanbukhari12345@gmail.com>
2024-08-13 16:50:35 +0500

    Made Some Changes ...Now Fully Functionally And Responsive

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-const celciusTem = document.getElementById('celciusTem');
-const farenhietTem = document.getElementById('farenhietTem');
-const kelvinTem = document.getElementById('kelvinTem');
+const celciusTem = document.getElementById("celciusTem");
+const farenhietTem = document.getElementById("farenhietTem");
+const kelvinTem = document.getElementById("kelvinTem");
 
-const calculate = document.getElementById('calculate');
-const back = document.getElementById('back');
+const faranhiet = document.getElementById("faranhiet");
+const kelvin = document.getElementById("kelvin");
 
-const faranhiet = document.getElementById('faranhiet');
-const kelvin = document.getElementById('kelvin');
+const calculate = document.getElementById("calculate");
+const back = document.getElementById("back");
+back.disabled = true;
 
-calculate.addEventListener('click',function(){
+let initialCalculationDone = false;
+
+calculate.addEventListener("click", function () {
+  calculateTemperature(celciusTem);
+});
+celciusTem.addEventListener("input", function () {
+  if (initialCalculationDone) {
     calculateTemperature(celciusTem);
+  }
+});
+back.addEventListener("click", function () {
+  faranhiet.style.display = "none";
+  kelvin.style.display = "none";
+  farenhietTem.value = null;
+  kelvinTem.value = null;
+  celciusTem.value = null;
+  initialCalculationDone = false;
+  calculate.disabled = false;
 });
 
-function calculateTemperature(temperature){
-    const numberValue = parseFloat(temperature.value);
-    const faranhietCal = (numberValue * 9/5) + 32;
-    const kelvinCal = numberValue + 273.15;
-    farenhietTem.value = faranhietCal;
-    kelvinTem.value = kelvinCal;
-    faranhiet.style.display = 'flex';
-    kelvin.style.display = 'flex';
+function calculateTemperature(temperature) {
+  if (temperature.value == "" || isNaN(temperature.value)) {
+    alert("First Enter Temperature For Conversion");
+    return;
+  }
+  const faranhietCal = (temperature.value * 9) / 5 + 32;
+  const kelvinCal = temperature.value + 273.15;
+  farenhietTem.value = faranhietCal;
+  kelvinTem.value = kelvinCal;
+  faranhiet.style.display = "flex";
+  kelvin.style.display = "flex";
+  calculate.disabled = true;
+  back.disabled = false;
+  initialCalculationDone = true;
 }
-back.addEventListener('click',function(){
-    faranhiet.style.display = 'none';
-    kelvin.style.display = 'none';
-    farenhietTem.value = null;
-    kelvinTem.value = null;
-    celciusTem.value = null;
-})
